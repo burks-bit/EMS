ccec9244779e1192152018b95f48b27b27c76101
unknown <bertnet08@gmail.com>
2024-02-09 12:08:55 +0800

    Added edit.blade.php, Bert

---
+++ resources/views/admin-hr/employees/edit.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', 'Employees')
+@section('content')
+
+<table style="width:100%;">
+    <thead>
+        <tr>
+            <td id="success_message" colspan="2"><td>
+        </tr>  
+        <tr>
+            <td style="text-align:left;"><h4>Edit Employee Details</h4><td>
+            <td style="text-align:right;">
+                <a href="#" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fa-solid fa-user-plus fa-lg"></i></a>
+            <td>
+        </tr>    
+    <thead>
+</table>
